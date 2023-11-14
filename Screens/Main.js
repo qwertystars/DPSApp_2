9fcd1af634ca7f75e7ed646dd28f09e698c540ce
PixelBearGames <gamedev.adrijo@gmail.com>
2023-11-15 00:35:07 +0530

    main page format created

--- Screens/Main.js
+++ Screens/Main.js
@@ -7,6 +7,7 @@ import {
   ImageBackground,
   Alert,
   Button,
+  ScrollView,
 } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 import React from "react";
@@ -16,18 +17,8 @@ import { Dimensions } from "react-native";
 import { useState, useEffect } from "react";
 import * as SecureStore from "expo-secure-store";
 
-
 export default function Main({ navigation }) {
-  //Simple addition of dates goes to next month
-
-  //console.log((temp1 - temp2) / (1000 * 60 * 60 * 24));
-  // console.log(today.toDateString());
-  // const newDate = new Date(today.toDateString());
-  // console.log(newDate.toDateString());
-
-    const MenuClicked = () => {
-        navigation.toggleDrawer();
-    }
+  let glucoseReadings = [];
 
   let today = new Date();
   let checkupDate = new Date();
@@ -54,10 +45,11 @@ export default function Main({ navigation }) {
     checkupDate.setHours(0, 0, 0);
     setCheckupDay(checkupDate);
     SetValueDB("checkupDate", checkupDate.toDateString());
-    setRemainingDays(checkupDuration);
+    setRemainingDays(checkupDuration - 1);
   }
 
-  SecureStore.deleteItemAsync("checkupDate");
+  //SecureStore.deleteItemAsync("checkupDate");
+  SecureStore.deleteItemAsync("glucoseReadings");
 
   useEffect(() => {
     today = new Date();
@@ -73,6 +65,14 @@ export default function Main({ navigation }) {
         );
       }
     });
+
+    GetValueDB("glucoseReadings").then((value) => {
+      if (value == "") {
+      } else {
+        glucoseReadings = value.split(",");
+        console.log(glucoseReadings);
+      }
+    });
   }, []);
 
   useEffect(() => {
@@ -80,7 +80,6 @@ export default function Main({ navigation }) {
       setShowAlert(true);
     }
   }, [remainingDays]);
-
 
   return (
     <SafeAreaView
@@ -94,34 +93,15 @@ export default function Main({ navigation }) {
         style={{
           marginHorizontal: 12,
           flexDirection: "row",
-          justifyContent: "flex-start",
+          justifyContent: "center",
         }}
-          >
-        <TouchableOpacity
-            style={{
-                height: 40,
-                width: 40,
-                backgroundColor: "rgba(255,255,255,0)",
-            }}
-            onPress={MenuClicked}
-        >
-            <MaterialIcons
-                name="menu"
-                size={24}
-                style={{
-                    position: "absolute",
-                    paddingLeft: 0,
-                    paddingTop: 0,
-                    color: "rgba(170, 219, 255, 0.87)",
-                }}
-            />
-        </TouchableOpacity>
+      >
         <Text
           style={{
             fontSize: 20,
             fontFamily: "sans-serif",
             fontWeight: "bold",
-            paddingLeft: "30%",
+            //paddingLeft: "43%",
           }}
         >
           Home
@@ -133,134 +113,348 @@ export default function Main({ navigation }) {
         resizeMode="cover"
         style={{
           flex: 1,
-          //justifyContent: "center",
         }}
       >
-        {/* <View
+        <View
           style={{
             position: "absolute",
-            height: 60,
-            width: 60,
-            backgroundColor: "#FFF",
+            height: "100%",
+            width: "100%",
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
+            zIndex: 1,
+            paddingTop: 180,
+            display: "none",
           }}
-        ></View> */}
-
-        <View style={{ paddingTop: 10, display: showAlert ? "flex" : "none" }}>
+        >
           <View
             style={{
-              height: 50,
-              width: "98%",
-              backgroundColor: "rgba(250,62,62,0.7)",
-              borderRadius: 15,
-              borderColor: "#96333d",
-              borderWidth: 3.5,
+              height: "40%",
+              width: "70%",
+              backgroundColor: "#FFF",
               alignSelf: "center",
+              paddingTop: 30,
             }}
-          >
-            <Text style={{
-             fontFamily: "sans-serif",
-             fontWeight: "bold",
-             fontSize: 22,
-             paddingTop: "1%",
-             paddingLeft: "5%",
-             color: "rgba(255,198,196,1)",
-             }}>
-             Get checkup done today!!
-            </Text>
+          ></View>
+        </View>
 
-            <TouchableOpacity
+        <ScrollView>
+          <View
+            style={{ paddingTop: 10, display: showAlert ? "flex" : "none" }}
+          >
+            <View
               style={{
-                position: "absolute",
-                paddingTop: 5,
-                paddingLeft: "83%",
-              }}
-              onPress={() => {
-                setShowAlert(false);
-                ResetCheckupDuration();
+                height: 50,
+                width: "98%",
+                backgroundColor: "rgba(250,62,62,0.7)",
+                borderRadius: 15,
+                borderColor: "#96333d",
+                borderWidth: 3.5,
+                alignSelf: "center",
               }}
             >
-              <View
+              <Text
                 style={{
-                  height: 30,
-                  width: 50,
-                  backgroundColor: "rgba(255,198,196,1)",
-                  borderRadius: 10,
-                }}
-              >
-                <Text style={{
                   fontFamily: "sans-serif",
                   fontWeight: "bold",
-                  fontSize: 16,
-                  paddingVertical: 4,
-                  alignSelf: "center",
-                  color: "#96333d",
-                }}>DONE</Text>
-              </View>
-            </TouchableOpacity>
-          </View>
-        </View>
-        {/* <LineChart
-          data={{
-            labels: ["January", "February", "March", "April", "May", "June"],
-            datasets: [
-              {
-                data: [
-                  Math.random() * 100,
-                  Math.random() * 100,
-                  Math.random() * 100,
-                  Math.random() * 100,
-                  Math.random() * 100,
-                  Math.random() * 100,
-                ],
-              },
-            ],
-          }}
-          width={(Dimensions.get("window").width * 80) / 100} // from react-native
-          height={220}
-          yAxisLabel="$"
-          yAxisSuffix="k"
-          yAxisInterval={1} // optional, defaults to 1
-          chartConfig={{
-            backgroundColor: "#e26a00",
-            backgroundGradientFrom: "#fb8c00",
-            backgroundGradientTo: "#ffa726",
-            decimalPlaces: 2, // optional, defaults to 2dp
-            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-            labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-            style: {
-              borderRadius: 16,
-            },
-            propsForDots: {
-              r: "6",
-              strokeWidth: "2",
-              stroke: "#ffa726",
-            },
-          }}
-          style={{
-            marginVertical: 8,
-            borderRadius: 16,
-            paddingTop: 20,
-          }}
-        /> */}
-        <Text
-          style={{
-            fontSize: 50,
-            alignSelf: "center",
-            color: "rgba(170, 219, 255, 0.87)",
-          }}
-        >
-          {remainingDays}
-        </Text>
-        <Text
-          style={{
-            fontSize: 25,
-            alignSelf: "center",
-            color: "rgba(170, 219, 255, 0.87)",
-          }}
-        >
-          Days till next checkup
+                  fontSize: 22,
+                  paddingTop: "1%",
+                  paddingLeft: "5%",
+                  color: "rgba(255,198,196,1)",
+                }}
+              >
+                Get checkup done today!!
               </Text>
-       </ImageBackground>
+
+              <TouchableOpacity
+                style={{
+                  position: "absolute",
+                  paddingTop: 5,
+                  paddingLeft: "83%",
+                }}
+                onPress={() => {
+                  setShowAlert(false);
+                  ResetCheckupDuration();
+                }}
+              >
+                <View
+                  style={{
+                    height: 30,
+                    width: 50,
+                    backgroundColor: "rgba(255,198,196,1)",
+                    borderRadius: 10,
+                  }}
+                >
+                  <Text
+                    style={{
+                      fontFamily: "sans-serif",
+                      fontWeight: "bold",
+                      fontSize: 16,
+                      paddingVertical: 4,
+                      alignSelf: "center",
+                      color: "#96333d",
+                    }}
+                  >
+                    DONE
+                  </Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+          </View>
+
+          <Text
+            style={{
+              fontSize: 50,
+              alignSelf: "center",
+              color: "rgba(170, 219, 255, 0.87)",
+            }}
+          >
+            {remainingDays}
+          </Text>
+          <Text
+            style={{
+              fontSize: 25,
+              alignSelf: "center",
+              color: "rgba(170, 219, 255, 0.87)",
+            }}
+          >
+            Days till next checkup
+          </Text>
+
+          <View
+            style={{
+              height: 260,
+            }}
+          >
+            <LineChart
+              data={{
+                //labels: ["January", "February", "March", "April", "May", "June"],
+                datasets: [
+                  {
+                    data: [
+                      Math.random() * 100,
+                      Math.random() * 100,
+                      Math.random() * 100,
+                      Math.random() * 100,
+                      Math.random() * 100,
+                      Math.random() * 100,
+                    ],
+                  },
+                ],
+              }}
+              width={(Dimensions.get("window").width * 75) / 100} // from react-native
+              height={190}
+              yAxisLabel="$"
+              yAxisSuffix="k"
+              yAxisInterval={1} // optional, defaults to 1
+              chartConfig={{
+                backgroundColor: "#e26a00",
+                backgroundGradientFrom: "#fb8c00",
+                backgroundGradientTo: "#ffa726",
+                decimalPlaces: 2, // optional, defaults to 2dp
+                color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                style: {
+                  borderRadius: 16,
+                },
+                propsForDots: {
+                  r: "6",
+                  strokeWidth: "2",
+                  stroke: "#ffa726",
+                },
+              }}
+              style={{
+                marginVertical: 8,
+                borderRadius: 16,
+                paddingTop: 15,
+                paddingLeft: 10,
+              }}
+            />
+
+            <View
+              style={{
+                position: "absolute",
+                paddingTop: 60,
+                paddingLeft: 332,
+              }}
+            >
+              <TouchableOpacity
+                style={{
+                  height: 50,
+                  width: 50,
+                  backgroundColor: "#FFF",
+                }}
+              ></TouchableOpacity>
+            </View>
+
+            <View
+              style={{
+                position: "absolute",
+                paddingTop: 120,
+                paddingLeft: 332,
+              }}
+            >
+              <TouchableOpacity
+                style={{
+                  height: 50,
+                  width: 50,
+                  backgroundColor: "#F3E",
+                }}
+              ></TouchableOpacity>
+            </View>
+
+            <View
+              style={{
+                position: "absolute",
+                paddingTop: 225,
+                paddingLeft: 20,
+              }}
+            >
+              <TouchableOpacity
+                style={{
+                  height: 40,
+                  width: 290,
+                  backgroundColor: "#623",
+                }}
+              ></TouchableOpacity>
+            </View>
+          </View>
+
+          <View
+            style={{
+              height: 260,
+            }}
+          >
+            <LineChart
+              data={{
+                //labels: ["January", "February", "March", "April", "May", "June"],
+                datasets: [
+                  {
+                    data: [
+                      Math.random() * 100,
+                      Math.random() * 100,
+                      Math.random() * 100,
+                      Math.random() * 100,
+                      Math.random() * 100,
+                      Math.random() * 100,
+                    ],
+                  },
+                ],
+              }}
+              width={(Dimensions.get("window").width * 75) / 100} // from react-native
+              height={190}
+              yAxisLabel="$"
+              yAxisSuffix="k"
+              yAxisInterval={1} // optional, defaults to 1
+              chartConfig={{
+                backgroundColor: "#e26a00",
+                backgroundGradientFrom: "#fb8c00",
+                backgroundGradientTo: "#ffa726",
+                decimalPlaces: 2, // optional, defaults to 2dp
+                color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                style: {
+                  borderRadius: 16,
+                },
+                propsForDots: {
+                  r: "6",
+                  strokeWidth: "2",
+                  stroke: "#ffa726",
+                },
+              }}
+              style={{
+                marginVertical: 8,
+                borderRadius: 16,
+                paddingTop: 15,
+                paddingLeft: 10,
+              }}
+            />
+
+            <View
+              style={{
+                position: "absolute",
+                paddingTop: 60,
+                paddingLeft: 332,
+              }}
+            >
+              <TouchableOpacity
+                style={{
+                  height: 50,
+                  width: 50,
+                  backgroundColor: "#FFF",
+                }}
+              ></TouchableOpacity>
+            </View>
+
+            <View
+              style={{
+                position: "absolute",
+                paddingTop: 120,
+                paddingLeft: 332,
+              }}
+            >
+              <TouchableOpacity
+                style={{
+                  height: 50,
+                  width: 50,
+                  backgroundColor: "#F3E",
+                }}
+              ></TouchableOpacity>
+            </View>
+
+            <View
+              style={{
+                position: "absolute",
+                paddingTop: 225,
+                paddingLeft: 20,
+              }}
+            >
+              <TouchableOpacity
+                style={{
+                  height: 40,
+                  width: 290,
+                  backgroundColor: "#623",
+                }}
+              ></TouchableOpacity>
+            </View>
+          </View>
+
+          <View
+            style={{
+              paddingTop: 30,
+            }}
+          >
+            <TouchableOpacity
+              style={{
+                height: 40,
+                width: "89%",
+                alignSelf: "center",
+                backgroundColor: "#FFF",
+              }}
+            />
+          </View>
+
+          <View
+            style={{
+              paddingTop: 15,
+            }}
+          >
+            <TouchableOpacity
+              style={{
+                height: 40,
+                width: "89%",
+                alignSelf: "center",
+                backgroundColor: "#FFF",
+              }}
+            />
+          </View>
+
+          <View
+            style={{
+              height: 130,
+            }}
+          />
+        </ScrollView>
+      </ImageBackground>
     </SafeAreaView>
   );
 }
