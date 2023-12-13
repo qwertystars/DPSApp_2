eb5e65a78d87f5dc72592f1f850e00f1ba1c157f
Good-Game-Great-Game <DPS.AdrijoIsGreat@gmail.com>
2023-12-13 19:37:48 +0530

    dsahbdlsyidubf

--- App.js
+++ App.js
@@ -1,7 +1,20 @@
+import "react-native-gesture-handler";
 import { Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Profile } from "./Screens";
 
-//SplashScreen.hideAsync();
-// SplashScreen.preventAutoHideAsync();
+const Stack = createNativeStackNavigator();
 export default function App() {
-  return <Text>Aviral is gay</Text>;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Profile">
+        <Stack.Screen
+          name="Profile"
+          component={Profile}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
